d8b378bffce24714debdfc449b0980995adc5a89
tuutuu <tuure.piitulainen@gmail.com>
2016-03-13 01:56:47 +0200

    database.update()

--- Tikape/src/main/java/tikape/Database.java
+++ Tikape/src/main/java/tikape/Database.java
@@ -13,6 +13,21 @@ public class Database<T> {
     public Database(String address) throws Exception {
         this.connection = DriverManager.getConnection(address);
     }
+    
+    public int update(String updateQuery) throws SQLException {
+        Statement stmt = connection.createStatement();
+        int changes = stmt.executeUpdate(updateQuery);
+
+        if(debug) {
+            System.out.println("---");
+            System.out.println(updateQuery);
+            System.out.println("Changed rows: " + changes);
+            System.out.println("---");
+        }
+        stmt.close();
+
+        return changes;
+    }
 
     public void setDebugMode(boolean d) {
         debug = d;
--- Tikape/src/main/java/tikape/Main.java
+++ Tikape/src/main/java/tikape/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Database database = new Database("jdbc:sqlite:Tikape_sqlite.db");
         database.setDebugMode(true);
         
+//      database.update("INSERT INTO Avaus (avaus_id, alue_id, avausnimi, avausteksti, aloitusaika) VALUES (4, 2, 'Onks ohjelmointi vaikeeta?', 'Onko?', CURRENT_TIMESTAMP)");
+        
         List<Avaus> avaukset = database.queryAndCollect("SELECT * FROM Avaus", new AvausCollector());
 
         for (Avaus avaus : avaukset) {
