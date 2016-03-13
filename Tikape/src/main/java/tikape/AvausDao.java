28a294a57728b9dfac8627d099890b277f533985
tmkau <tuuli.kauppala@helsinki.fi>
2016-03-13 02:46:51 +0200

    02:46

---
+++ Tikape/src/main/java/tikape/AvausDao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape;
+
+
+
+import java.util.*;
+import java.sql.*;
+
+public class avausDao implements Dao<Avaus, Integer> {
+
+    @Override
+    public Avaus findOne(Integer key) throws SQLException {
+        // ei toteutettu
+        return null;
+    }
+
+    @Override
+    public List<Avaus> findAll() throws SQLException {
+	// ei toteutettu
+	return null;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
+}
