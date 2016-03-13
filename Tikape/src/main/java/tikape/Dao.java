e27cf3f1b6918b86e11cae894ac54744c2297221
tmkau <tuuli.kauppala@helsinki.fi>
2016-03-13 02:28:43 +0200

    jotain 0228_13_3

---
+++ Tikape/src/main/java/tikape/Dao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape;
+
+
+import java.sql.*;
+import java.util.*;
+
+public interface Dao<T, K> {
+
+    T findOne(K key) throws SQLException;
+
+    List<T> findAll() throws SQLException;
+
+    void delete(K key) throws SQLException;
+}
